ae7e9b812eeb1c0f8b546d99d244de6a6af58ca2
Katleiah <katleiahramos@gmail.com>
2018-03-07 19:34:04 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,3 @@
 def reverse_each_word(statement)
-  reversed_words = ""
-  statement.each do |character|
-    reversed_words << character
-  end
-  return reversed_words
+  statement.split(" ")
 end 
